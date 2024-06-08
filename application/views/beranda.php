1e7fe17df7b26bdbb9506711a4833b9df3515861
alkha77 <kuliah.alvi@gmail.com>
2024-06-08 19:22:09 +0700

    first commit

---
+++ application/views/beranda.php
@@ -0,0 +1,5 @@
+<div class="container">
+        <div class="content">
+            <h1>Tugas WebPro</h1>
+            <h2>tampilan Home</h2>
+        </div>
---
+++ application/views/biodata-saya.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Biodata Pribadi</title>
+	<link rel="stylesheet" href="assets/css/form.css">
+	<style>
+		body {
+			font-family: Poppins, sans-serif;
+			margin: 0;
+			padding: 0;
+			background: url("https://i.pinimg.com/originals/a2/fe/21/a2fe21da9ce7d1698f5b48cdb506c853.jpg");
+			background-size: cover;
+		}
+
+		.container {
+			max-width: 600px;
+			margin: 120px auto;
+			padding: 20px;
+			background-color: #1f242d;
+			border-radius: 8px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+		h2 {
+			color: white;
+		}
+
+		.profile-img {
+			display: block;
+			margin: 0 auto 20px;
+			width: 200px;
+			height: 200px;
+			border-radius: 50%;
+			object-fit: cover;
+		}
+
+		.info {
+			color: white;
+			text-align: center;
+		}
+
+		.info p {
+            font-size: 16px;
+			margin: 10px 0;
+		}
+
+	</style>
+</head>
+
+<body>
+	<div class="container">
+        <h2>Data Mahasiswa</h2>
+		<img src="assets/img/alvi.jpeg" alt="Foto Profil" class="profile-img">
+		<div class="info">
+			<p><strong>Nama:</strong> Alvi Khairullah Andhika</p>
+            <p><strong>Kelas:</strong> 17.6A.26</p>
+			<p><strong>Email:</strong> Kuliah.alvi@gmail.com</p>
+			<p><strong>Hobi:</strong> Bermain Game, membaca novel</p>
+		</div>
+	</div>
+</body>
+
+</html>
